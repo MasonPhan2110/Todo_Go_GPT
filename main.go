fbc2a7cdf25facf7842d226e24de4dd16cf588b9
Mason Phan <phannhatminh1099@gmail.com>
2023-10-24 16:06:57 +0700

    add swagger

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	db "MasonPhan2110/Todo_Go_GPT/db/setup"
-	"MasonPhan2110/Todo_Go_GPT/middleware"
+	"MasonPhan2110/Todo_Go_GPT/docs"
 	"MasonPhan2110/Todo_Go_GPT/pkg/setting"
 	"MasonPhan2110/Todo_Go_GPT/routes"
 	"MasonPhan2110/Todo_Go_GPT/utils"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,6 +21,17 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.Use(middleware.AuthMiddleware(setting.AppSetting.TokenMaker))
+
 	routes.AddRoutes(r)
+
+	docs.SwaggerInfo.Title = "Swagger Todo List"
+	docs.SwaggerInfo.Description = "Todo List Swagger"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "0.0.0.0:8080"
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
+	if err := r.Run("0.0.0.0:8080"); err != nil {
+		fmt.Println("Error in router run: ", err)
+	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,13 +4,20 @@ import (
 	"MasonPhan2110/Todo_Go_GPT/api"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func AddRoutes(c *gin.Engine) {
 	v1 := c.Group("api/v1")
 
+	v1.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	user := v1.Group("user")
 	{
 		user.POST("login", api.Login)
 	}
+
+	// r.Use(middleware.AuthMiddleware(setting.AppSetting.TokenMaker))
+
 }
