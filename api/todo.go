b2a4c99eb3e0b731e535115ed0bfb72038e833ca
Mason Phan <phannhatminh1099@gmail.com>
2023-11-02 11:34:24 +0700

    add ListTasks

--- api/todo.go
+++ api/todo.go
@@ -36,6 +36,18 @@ func newTaskResponse(task db.Todo) createTaskResponse {
 	}
 }
 
+// CreateTask godoc
+//
+//	@Summary		Create new Task to Todo
+//	@Tags			todo
+//	@Accept			json
+//	@Produce		json
+//	@Param			request body createTaskRequest true "query params"
+//	@Failure		400	{object}	utils.HTTPError
+//	@Failure		404	{object}	utils.HTTPError
+//	@Failure		500	{object}	utils.HTTPError
+//	@Success		200	{object}	createTaskResponse
+//	@Router			/api/v1/todo/create [post]
 func (server *Server) CreateTask(ctx *gin.Context) {
 	var req createTaskRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
@@ -64,7 +76,45 @@ func (server *Server) CreateTask(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, rsp)
 }
 
-func (server *Server) GetTask(ctx *gin.Context) {}
+type getTaskRequest struct {
+	UserID int64 `json:"user_id" binding:"required"`
+	Limit  int32 `json:"limit" binding:"required"`
+	Offset int32 `json:"offset" binding:"required"`
+}
+
+type getTaskResponse struct {
+	UserID      int64     `json:"user_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      bool      `json:"status"`
+	Deadline    time.Time `json:"deadline"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+func (server *Server) GetTasks(ctx *gin.Context) {
+	var req getTaskRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		utils.NewError(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	arg := db.ListTasksParams{
+		UserID: req.UserID,
+		Limit:  req.Limit,
+		Offset: req.Offset,
+	}
+
+	listTasks, err := db.DBStore.ListTasks(ctx, arg)
+	if err != nil {
+		if db.ErrorCode(err) == db.UniqueViolation {
+			ctx.JSON(http.StatusForbidden, utils.ErrorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, utils.ErrorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, listTasks)
+}
 
 func (server *Server) UpdateTask(ctx *gin.Context) {}
 
